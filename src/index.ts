ae71d7c973a1bfbba58d23ef5c9c01f3e887f7a5
alator21 <botazz133@gmail.com>
2025-01-17 22:03:41 +0200

    Expose track.updateNowPlaying

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export { getSession } from './api/auth/getSession.ts'
 // export { love } from './api/track/love.ts'
 export { scrobble } from './api/track/scrobble.ts'
 // export { unlove } from './api/track/unlove.ts'
-// export { updateNowPlaying } from './api/track/updateNowPlaying.ts'
+export { updateNowPlaying } from './api/track/updateNowPlaying.ts'
 // user
 export { getTopTracks } from './api/user/getTopTracks.ts'
 export { getTopArtists } from './api/user/getTopArtists.ts'
