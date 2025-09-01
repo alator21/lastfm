19bb7895f2e5fad11b7ae81edd06f4730c2023d4
alator21 <botazz133@gmail.com>
2025-09-01 19:37:54 +0300

    More documentation

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,5 +1,13 @@
 import SparkMD5 from "spark-md5";
 
+/**
+ * Create an API signature for Last.fm API authentication.
+ * Sorts parameters alphabetically, concatenates with shared secret, and creates MD5 hash.
+ * @param params - Object containing API parameters as key-value pairs
+ * @param sharedSecret - Your Last.fm shared secret
+ * @returns MD5 hash signature for API authentication
+ * @internal This is an internal utility function used by API methods for authentication
+ */
 export function createSignature(params: Record<string, string>, sharedSecret: string): string {
   const sortedParams = Object.keys(params)
     .sort()
